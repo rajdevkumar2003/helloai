293b46ea7964f5ad2d0ccc380741af6b6bf4d8cc
Rajeev <rajeevkumarjbo2003@gmail.com>
2025-08-14 11:03:52 +0530

    01:setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+    <Button variant="custom">
+      Click Me
+    </Button>
+    </>
+  );
+}
